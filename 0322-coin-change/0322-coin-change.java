b183f48d0cb4aec301ac22162df2a20c5b5df809
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-05-19 11:09:54 +0530

    Time: 18 ms (40.55%), Space: 47.2 MB (16.34%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,33 +1,25 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-
         int[][] dp = new int[n][amount + 1];
-
         for (int i = 0; i < n; i++) {
             Arrays.fill(dp[i], (int)1e9);
         }
-
         for (int j = 0; j <= amount; j++) {
             if (j % coins[0] == 0) {
                 dp[0][j] = j / coins[0];
             }
         }
-
         for (int i = 1; i < n; i++) {
             for (int j = 0; j <= amount; j++) {
-
                 int notTake = dp[i - 1][j];  
                 int take = (int)1e9;
-
                 if (coins[i] <= j) {
                     take = 1 + dp[i][j - coins[i]]; 
                 }
-
                 dp[i][j] = Math.min(take, notTake);
             }
         }
-
         int ans = dp[n - 1][amount];
         return ans >= 1e9 ? -1 : ans;
     }
