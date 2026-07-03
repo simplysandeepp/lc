f661a43ab0b57d7d32cd6b825f25cd687beb07ca
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2026-07-03 20:25:11 +0530

    Time: 0 ms (100%), Space: 42 MB (75.69%) - LeetHub

--- 0400-nth-digit/0400-nth-digit.java
+++ 0400-nth-digit/0400-nth-digit.java
@@ -1,14 +1,18 @@
 class Solution {
-    public int findNthDigit(int n) {
-        int len = 1; long count = 9; int start = 1;
-        while (n > len * count ){
-            n -= len * count;
-            len += 1;
-            count *= 10;
-            start *= 10;
+    public int findNthDigit(int n){
+        long digitlength =1;
+        int start=1;
+        int end=9;
+        while(n > digitlength*end){
+            n -= digitlength*end;
+            digitlength++;
+            start*=10;
+            end*=10;
         }
-        start += (n-1) / len;
-        String s = Integer.toString(start);
-        return Character.getNumericValue(s.charAt((n - 1) % len));
+        start += (n-1)/digitlength;
+        String number = String.valueOf(start);
+        int index = (int)((n-1) % digitlength); 
+        return number.charAt(index) - '0';
+    
     }
 }
